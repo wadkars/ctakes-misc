206c7a5f6985c47f3c63c13073de8a0f01c4cda8
wadkars <56367624+wadkars@users.noreply.github.com>
2019-11-07 08:20:53 -0500

    Update convert_to_sequence_files.sh
    Wrong package

--- scripts/convert_to_sequence_files.sh
+++ scripts/convert_to_sequence_files.sh
@@ -2,7 +2,7 @@ echo 'RAW_FILES_PATH=' $1
 echo 'SEQUENCE_FILE_PATH=' $2
 echo 'NO_OF_REDUCERS=' $3
 echo 'MAX_FILE_SPLIT_SIZE' = $4
-hadoop jar ctakes-misc-4.0.0-jar-with-dependencies.jar com.cloudera.SmallFilesToSequenceFile \
+hadoop jar ctakes-misc-4.0.0-jar-with-dependencies.jar com.cloudera.ctakes.SmallFilesToSequenceFile \
 -D mapreduce.map.memory.mb=2000 \
 -D mapreduce.reduce.memory.mb=2000 \
 -D mapreduce.map.java.opts.max.heap=1800 \
